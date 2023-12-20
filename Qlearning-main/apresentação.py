9f24df8db8ce8e364c3cb8a184b0bb25ee579d2b
guilp03 <108680176+guilp03@users.noreply.github.com>
2023-12-19 21:49:38 -0300

    projeto final

--- Qlearning-main/apresentação.py
+++ Qlearning-main/apresentação.py
@@ -1,4 +1,13 @@
-[[-68.95073535560041, -110.70178420929071, -111.5743432146406], 
+import numpy as np
+from connection import connect
+from connection import get_state_reward
+
+s = connect(2037)
+n_actions = 3
+n_states = 24
+
+#tabela que guarda as recompensas de cada estado e ação
+Q_table = [[-68.95073535560041, -110.70178420929071, -111.5743432146406], 
 [-170.21309729286054, -110.28620793725995, -111.10561827996658], 
 [-159.269452, -112.70309232385725, -112.40731923380218], 
 [-168.287371, -113.7284543751043, -102.19206381292622], 
@@ -94,3 +103,60 @@
 [-14.6975102, 19.37179129395043, -0.1], 
 [-0.2, -0.16395186, 27.804185413931442], 
 [194.70628511832243, -0.1, 16.903842142856316]]
+
+#número de vezes que ele vai chegar ao final
+n_episodes = 100
+
+#número de tentativas que ele tem a cada episode de chegar ao final
+max_iter_episode = 1000
+
+#probabilidade de ele explorar algo novo e não se atentar necessariamente ao melhor valor conhecido
+exploration_prob = 0.05
+
+#uma vez que ele tenha ja conseguido chegar ao final uma vez, a necessidade de explorar diminui
+exploration_decreasing_decay = 0.0
+
+#teoricamente existe um minimo de exploração que precisa ter
+min_exploration_prob = 0.0
+
+#discount factor
+gamma = 0.99
+
+#learning rate
+alpha = 0.1
+
+#quanto custou o episode
+total_rewards_episode = []
+
+for i in range(n_episodes):
+    current_state = "0b00000000"
+    done = False
+    
+    total_episode_reward = 0
+    
+    for j in range(max_iter_episode):
+        action_names = ["jump", "left", "right"]
+        if np.random.uniform(0, 1) < exploration_prob:
+            action = np.random.choice(action_names)
+            print("random")
+        else:
+            action = action_names[np.argmax(Q_table[int(current_state, 2)][:])]
+            print("consciente")
+        next_state, reward = get_state_reward(s, action)
+        
+        if reward == 300:
+            done = True
+            
+        Q_table[int(current_state, 2)][action_names.index(action)] = (1-alpha) * Q_table[int(current_state, 2)][action_names.index(action)] + alpha * (reward + gamma * max(Q_table[int(next_state, 2)]))
+        total_episode_reward = total_episode_reward + reward
+        
+        if done:
+            break
+        current_state = next_state
+        #print(Q_table)
+        print(i,j)
+        print(exploration_prob)
+    #exploration_prob = max(min_exploration_prob, np.exp(-exploration_decreasing_decay*i))
+    total_rewards_episode.append(total_episode_reward)
+print(Q_table)
+
